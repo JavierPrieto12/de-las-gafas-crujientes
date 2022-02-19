35de4145baa067413345fc3f0da812eff619e025
JavierPrieto12 <99421914+JavierPrieto12@users.noreply.github.com>
2022-02-18 17:07:03 -0700

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_holamundo;
+
+/**
+ *
+ * @author Owner
+ */
+public class EVA1_1_HOLAMUNDO {//CUERPO DE UNA CLASE
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//FUNCION
+        // TODO code application logic here
+        System.out.println("Hello world!!");
+    }
+    
+}
