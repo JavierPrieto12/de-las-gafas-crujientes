2864898dee780778601ee6ee06fea0fc45131f17
JavierPrieto12 <99421914+JavierPrieto12@users.noreply.github.com>
2022-05-06 09:44:44 -0600

    Add files via upload

---
+++ EVA3_3_DIVISION_CERO/src/eva3_3_division_cero/EVA3_3_DIVISION_CERO.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva3_3_division_cero;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_3_DIVISION_CERO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Introduce un numero entero 1: ");
+        int entero = input.nextInt();
+        
+        System.out.println("Introduce un numero entero 2: ");
+        int entero2 = input.nextInt();
+        
+        System.out.println("Divicion: " + (entero/entero2));
+        
+    }
+    
+}
